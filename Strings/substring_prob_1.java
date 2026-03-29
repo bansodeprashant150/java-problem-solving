595c3ae57648c5323417b3b812a5d993827be270
bansodeprashant150 <bansodeprashant150@gmail.com>
2026-03-29 15:01:19 +0530

    Add substring example in substring_prob_1.java

---
+++ Strings/substring_prob_1.java
@@ -0,0 +1,7 @@
+public class substring_prob_1 {
+    public static void main(String[] args) {
+        String s = "HelloWorld";
+        String sub = s.substring(3, 7);
+        System.out.println(sub);
+    }
+}
