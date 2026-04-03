edc41efe4f137e76a1d0b911211dee7eea4dc7d7
bansodeprashant150 <bansodeprashant150@gmail.com>
2026-04-03 23:07:55 +0530

    Create one_D_array.java

---
+++ Arrays/one_D_array.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class one_D_array {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+
+        int n=scan.nextInt();
+
+        int[]a=new int[n];
+
+
+        for(int i=0;i<n;i++){
+            a[i]=scan.nextInt();
+        }
+
+System.out.print("Output is: ");
+        for(int i=0; i<n;i++){
+            
+            System.out.print(a[i]+" ");
+        }
+        scan.close();
+    }
+}
